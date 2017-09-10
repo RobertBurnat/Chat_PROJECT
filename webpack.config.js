6ec542aac278f401e553523c7b38bdaeb4d10ec4
Robert Burnat <burnat.robertt@gmail.com>
2017-09-10 21:01:07 +0200

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     ],
      output: {
         filename: './bundle.js',
-        path: resolve(__dirname, '/public')
+        path: path.resolve(__dirname, '/public')
      },
      module: {
         rules: [
